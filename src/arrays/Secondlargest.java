eb56cc8da3dad9d17d6b06062aa737b1b9d28b22
adnan <mdajahangir2005@gmail.com>
2026-08-13 22:22:32 +0530

    secondLargest brute

---
+++ src/arrays/Secondlargest.java
@@ -0,0 +1,25 @@
+
+package arrays;
+
+import java.util.Arrays;
+
+public class Secondlargest {
+
+    class Solution{
+        static int brute(int [] arr){
+            Arrays.sort(arr);
+            return arr[arr.length-2];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr1 = {2,5,1,3,0};
+        int[] arr2 = {8, 10, 5, 7, 9};
+
+
+        System.out.println("The Largest element in the array is: " + Solution.brute(arr1));
+        System.out.println("The Largest element in the array is: " + Solution.brute(arr2));
+
+    }
+}

5cfce604403306927f96afe39b97700622a3e0e3
adnan <mdajahangir2005@gmail.com>
2026-08-17 23:15:46 +0530

    findmax-optimal

--- src/arrays/Findmax.java
+++ src/arrays/Findmax.java
@@ -9,16 +9,34 @@ public class Findmax {
             Arrays.sort(arr);
             return arr[arr.length-1];
         }
+
+
+        static int optimal(int [] arr){
+            int max = arr[0];
+            for(int i =1;i<arr.length;i++)
+            {
+                if(arr[i]>max)
+                {
+                    max=arr[i];
+                }
+            }
+            return max;
+
+
+        }
     }
 
 
     public static void main(String[] args) {
         int [] arr1 = {2,5,1,3,0};
-        int[] arr2 = {8, 10, 5, 7, 9};
+        int[] arr2 = {8, 13, 5, 7, 9};
 
 
         System.out.println("The Largest element in the array is: " + Solution.brute(arr1));
         System.out.println("The Largest element in the array is: " + Solution.brute(arr2));
+
+        System.out.println("The Largest element in the array is: " + Solution.optimal(arr1));
+        System.out.println("The Largest element in the array is: " + Solution.optimal(arr2));
 
     }
 }

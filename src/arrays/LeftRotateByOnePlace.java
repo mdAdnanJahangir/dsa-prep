bc864c49c17278946ec6fc3a5322a7ea2b107daa
adnan <mdajahangir2005@gmail.com>
2026-08-21 23:14:05 +0530

    LeftRotateByOnePlace  optimal

--- src/arrays/LeftRotateByOnePlace.java
+++ src/arrays/LeftRotateByOnePlace.java
@@ -21,6 +21,23 @@ public class LeftRotateByOnePlace {
             }
             System.out.println();
         }
+
+
+        public  static void   optimal (int[] nums) {
+            int temp = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                nums[i - 1] = nums[i];
+            }
+
+            nums[nums.length - 1] = temp;
+        }
+
+
+
+
+
+
     }
 
 
@@ -30,7 +47,9 @@ public class LeftRotateByOnePlace {
     public static void main(String[] args) {
         int n = 5;
         int[] arr = {1, 2, 3, 4, 5};
-
         Solution.brute(arr, n);
+        Solution.optimal(arr);
+        System.out.println(arr[n-1]);
+
     }
 }

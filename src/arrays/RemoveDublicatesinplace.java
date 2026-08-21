62d10ce8b347cae90e98e7452c8ef33cf680127b
adnan <mdajahangir2005@gmail.com>
2026-08-21 23:00:48 +0530

    RemoveDublicatesinplace brute

---
+++ src/arrays/RemoveDublicatesinplace.java
@@ -0,0 +1,47 @@
+package arrays;
+import java.util.HashSet;
+
+
+    public class RemoveDublicatesinplace  {
+
+        private static class Solution
+        {
+
+            public static int brute(int[] nums) {
+
+                HashSet<Integer> seen = new HashSet<>();
+
+
+                int index = 0;
+
+
+                for (int num : nums) {
+
+
+                    if (!seen.contains(num)) {
+                        seen.add(num);
+                        nums[index] = num;
+                        index++;
+                    }
+                }
+
+                return index;
+            }
+
+
+                public static void main (String[] args){
+                    int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+
+                    int k = Solution.brute(nums);
+
+                    System.out.println("k = " + k);
+                    System.out.print("Array after removing duplicates: ");
+                    for (int i = 0; i < k; i++) {
+                        System.out.print(nums[i] + " ");
+                    }
+                }
+        }
+    }
+
+

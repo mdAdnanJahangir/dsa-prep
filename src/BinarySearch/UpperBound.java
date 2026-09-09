7b1428998d587be607fcf50e0af0cfd142da8ed6
adnan <mdajahangir2005@gmail.com>
2026-09-09 22:29:57 +0530

    UpperBound binary search

--- src/BinarySearch/UpperBound.java
+++ src/BinarySearch/UpperBound.java
@@ -3,6 +3,27 @@ package BinarySearch;
 
 
 class UpperBound {
+
+    static int upperBoundOptimal(int [] brr , int target ){
+        int low =0;
+        int high = brr.length-1;
+        int mid;
+        int ans = brr.length;
+        while(low <= high){
+            mid = (low+high)/2;
+            if(brr[mid] > target){
+                ans = mid;
+                high = mid -1;
+            }else{
+                low = mid +1;
+            }
+        }
+         return ans ;
+    }
+
+
+
+
     // Linear search method to find upper bound
     public static int upperBound(int[] arr, int x) {
         for (int i = 0; i < arr.length; i++) {
@@ -18,7 +39,11 @@ class UpperBound {
         int x = 9;
 
         int ind = upperBound(arr, x);
+        int ans  = upperBoundOptimal(arr, x);
+
 
         System.out.println("The upper bound is the index: " + ind); // Output result
+        System.out.println("The upper bound is the index: " + ans); // Output result
+
     }
 }

b58d92975fec484fa26dca1a3ebd05d1996272b4
adnan <mdajahangir2005@gmail.com>
2026-09-18 23:43:53 +0530

    LastOccourence

---
+++ src/BinarySearch/LastOccourence.java
@@ -0,0 +1,40 @@
+package BinarySearch;
+
+
+
+public class LastOccourence {
+
+    static int  Bs  (int[] brr,int target ){
+        int s =0,e=brr.length-1;
+        int mid ;
+        int ans =-1;
+
+        while(s<=e){
+
+            mid = (s+e)/2;
+
+            if(brr[mid] == target ){
+                ans = mid;
+                s = mid + 1;
+            } else if(brr[mid]  > target){
+                e = mid -1 ;
+            } else{
+                s =   mid +1;
+            }
+
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] arr ={2,3,4,4,4,4, 6};
+        int target = 4;
+
+        int  second = Bs(arr,target);
+        System.out.println(second);
+
+    }
+}
